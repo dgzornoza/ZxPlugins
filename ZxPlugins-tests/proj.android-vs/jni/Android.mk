686fe3b8110b385b49d69081c2848bb38bc4c2e7
David González Zornoza <zelion_spn@hotmail.com>
2016-04-06 09:59:16 +0200

    compiled android project in VS2015

--- ZxPlugins-tests/proj.android-vs/jni/Android.mk
+++ ZxPlugins-tests/proj.android-vs/jni/Android.mk
@@ -2,22 +2,25 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 
-$(call import-add-path,$(LOCAL_PATH)/../../../cocos2d)
-$(call import-add-path,$(LOCAL_PATH)/../../../cocos2d/external)
-$(call import-add-path,$(LOCAL_PATH)/../../../cocos2d/cocos)
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d)
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/external)
+$(call import-add-path,$(LOCAL_PATH)/../../cocos2d/cocos)
 
 LOCAL_MODULE := cocos2dcpp_shared
 
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
 LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   ../../../Classes/AppDelegate.cpp \
-				   ../../../Classes/LayerBase.cpp \
-				   ../../../Classes/MainMenuLayer.cpp \
+                   ../../Classes/AppDelegate.cpp \
+				   ../../Classes/LayerBase.cpp \
+				   ../../Classes/MainMenuLayer.cpp \
+				   ../../Classes\cordova_plugin_device_test/cordova_plugin_device_test.cpp \
+				   ../../../ZxPlugins-lib/scr/PluginFactory.cpp \
 				   
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../../Classes \
-					$(LOCAL_PATH)/../../../../
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes \
+					$(LOCAL_PATH)/../../../
 
 # _COCOS_HEADER_ANDROID_BEGIN
 # _COCOS_HEADER_ANDROID_END
