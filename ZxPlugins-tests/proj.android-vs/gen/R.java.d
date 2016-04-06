2a0aad6ab586a432a9e154c12b6217cf8a66f8aa
David González Zornoza <zelion_spn@hotmail.com>
2016-04-06 11:15:26 +0200

    removed android project for VS 2015

--- ZxPlugins-tests/proj.android-vs/gen/R.java.d
+++
@@ -1,6 +0,0 @@
-D:\Projects\Libraries\ZxPlugins\ZxPlugins-tests\proj.android-vs\gen\com\dgzornoza\cocos2dx\zxplugins\R.java \
- : D:\Projects\Libraries\ZxPlugins\ZxPlugins-tests\proj.android-vs\res\drawable-hdpi\icon.png \
-D:\Projects\Libraries\ZxPlugins\ZxPlugins-tests\proj.android-vs\res\drawable-ldpi\icon.png \
-D:\Projects\Libraries\ZxPlugins\ZxPlugins-tests\proj.android-vs\res\drawable-mdpi\icon.png \
-D:\Projects\Libraries\ZxPlugins\ZxPlugins-tests\proj.android-vs\res\values\strings.xml \
-D:\Projects\Libraries\ZxPlugins\ZxPlugins-tests\proj.android-vs\bin\AndroidManifest.xml \
