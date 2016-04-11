32b83a4529b326df2aa28b14881f3aa2101c2e46
David González Zornoza <zelion_spn@hotmail.com>
2016-04-11 22:34:36 +0200

    Add android studio empty lib for plugin

---
+++ ZxPlugins-lib/platform/android/ZxPluginLib/src/main/java/com/dgzornoza/zxplugin/CommandHandler.java
@@ -0,0 +1,12 @@
+package com.dgzornoza.zxplugin;
+
+public class CommandHandler  {
+
+    public native void doTest();  // to be supplied in C++ trhough JNI
+
+    public void showId() {  // replace the previous version of example 5
+
+        doTest();         // <==== invoke the native method
+    }
+}
+
